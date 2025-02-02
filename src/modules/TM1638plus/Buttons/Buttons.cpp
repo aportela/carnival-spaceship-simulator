470f3b50f0319d11a9a0ed6a6832cac9bd9f07aa
alex <766f6964+github@gmail.com>
2025-02-02 15:23:01 +0100

    added debouncer && button enum mask values

--- src/modules/TM1638plus/Buttons/Buttons.cpp
+++ src/modules/TM1638plus/Buttons/Buttons.cpp
@@ -1,4 +1,5 @@
 #include "Buttons.hpp"
+#include "Arduino.h"
 
 Buttons::Buttons(TM1638plus *module) : module(module)
 {
@@ -9,20 +10,27 @@ Buttons::~Buttons()
     this->module = nullptr;
 }
 
-bool Buttons::loop(void)
+uint8_t Buttons::loop(void)
 {
-    uint8_t buttons = this->module->readButtons();
-    /* buttons contains a byte with values of button s8s7s6s5s4s3s2s1
-     HEX  :  Switch no : Binary
-     0x01 : S1 Pressed  0000 0001
-     0x02 : S2 Pressed  0000 0010
-     0x04 : S3 Pressed  0000 0100
-     0x08 : S4 Pressed  0000 1000
-     0x10 : S5 Pressed  0001 0000
-     0x20 : S6 Pressed  0010 0000
-     0x40 : S7 Pressed  0100 0000
-     0x80 : S8 Pressed  1000 0000
-    */
-    // Serial.println(buttons, HEX);
-    this->module->displayIntNum(buttons, true, TMAlignTextLeft);
+    uint64_t timestamp = millis();
+    if (timestamp - this->lastTimestamp > BUTTON_DEBOUNCE_MS)
+    {
+        this->lastTimestamp = timestamp;
+        /* buttons contains a byte with values of button s8s7s6s5s4s3s2s1
+        HEX  :  Switch no : Binary
+        0x01 : S1 Pressed  0000 0001
+        0x02 : S2 Pressed  0000 0010
+        0x04 : S3 Pressed  0000 0100
+        0x08 : S4 Pressed  0000 1000
+        0x10 : S5 Pressed  0001 0000
+        0x20 : S6 Pressed  0010 0000
+        0x40 : S7 Pressed  0100 0000
+        0x80 : S8 Pressed  1000 0000
+        */
+        return (this->module->readButtons());
+    }
+    else
+    {
+        return (0);
+    }
 }
--- src/modules/TM1638plus/Buttons/Buttons.hpp
+++ src/modules/TM1638plus/Buttons/Buttons.hpp
@@ -5,16 +5,31 @@
 
 #include <TM1638plus.h>
 
+#define BUTTON_DEBOUNCE_MS 128
+
+enum BUTTON_VALUE
+{
+    BUTTON_S1 = 0x01,
+    BUTTON_S2 = 0x02,
+    BUTTON_S3 = 0x04,
+    BUTTON_S4 = 0x08,
+    BUTTON_S5 = 0x10,
+    BUTTON_S6 = 0x20,
+    BUTTON_S7 = 0x40,
+    BUTTON_S8 = 0x80,
+};
+
 class Buttons
 {
 protected:
     TM1638plus *module = nullptr;
+    uint64_t lastTimestamp = 0;
 
 public:
     Buttons(TM1638plus *module);
     ~Buttons();
 
-    bool loop(void);
+    uint8_t loop(void);
 };
 
 #endif // TM1638_PLUS_BUTTONS_H
