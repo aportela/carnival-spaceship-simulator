3e6d531005e6f3453e85c66c093585eb21ee55d5
alex <766f6964+github@gmail.com>
2025-02-02 15:40:16 +0100

    added common function to check if refresh is required

--- src/modules/TM1638plus/Leds/LedEffect.cpp
+++ src/modules/TM1638plus/Leds/LedEffect.cpp
@@ -1,4 +1,5 @@
 #include "LedEffect.hpp"
+#include "Arduino.h"
 
 LedEffect::LedEffect(TM1638plus *module) : module(module)
 {
@@ -7,4 +8,18 @@ LedEffect::LedEffect(TM1638plus *module) : module(module)
 LedEffect::~LedEffect()
 {
     this->module = nullptr;
+}
+
+bool LedEffect::refresh(void)
+{
+    uint64_t timestamp = millis();
+    if (timestamp - this->lastRefresh > 64)
+    {
+        this->lastRefresh = timestamp;
+        return (true);
+    }
+    else
+    {
+        return (false);
+    }
 }
--- src/modules/TM1638plus/Leds/LedEffect.hpp
+++ src/modules/TM1638plus/Leds/LedEffect.hpp
@@ -9,12 +9,15 @@ class LedEffect : public ILedEffect
 {
 protected:
     TM1638plus *module = nullptr;
+    uint64_t lastRefresh = 0;
 
 public:
     LedEffect(TM1638plus *module);
     ~LedEffect();
 
     virtual bool loop(void) = 0;
+
+    bool refresh(void);
 };
 
 #endif // TM1638_PLUS_LED_EFFECT_H
