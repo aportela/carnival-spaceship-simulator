72cbc63d96754e9c548b8852aaf8fac4d19914a0
alex <766f6964+github@gmail.com>
2025-02-09 12:15:31 +0100

    re-enabled TM1638 display module

--- carnival-spaceship-simulator.ino
+++ carnival-spaceship-simulator.ino
@@ -1,10 +1,12 @@
 #include "src/modules/TM1638plus/ModuleTM1638plus.hpp"
-#include "src/modules/Audio/Synth.hpp"
 #include "src/modules/Audio/Sampler.hpp"
 
 ModuleTM1638plus *controlPanel = nullptr;
-Synth *synth = nullptr;
 Sampler *sampler = nullptr;
+
+#define STROBE_TM 4 // strobe
+#define CLOCK_TM 6  // clock
+#define DIO_TM 7    // data
 
 #define I2S_BCK_PIN 26  // Bit Clock
 #define I2S_LRCK_PIN 27 // Left Right Clock
@@ -13,9 +15,7 @@ Sampler *sampler = nullptr;
 void setup()
 {
     Serial.begin(9600);
-    delay(2000);
-    // controlPanel = new ModuleTM1638plus(STROBE_TM, CLOCK_TM, DIO_TM, true);
-    // synth = new Synth(I2S_BCK_PIN, I2S_LRCK_PIN, I2S_DATA_PIN);
+    controlPanel = new ModuleTM1638plus(STROBE_TM, CLOCK_TM, DIO_TM, true);
     sampler = new Sampler(I2S_BCK_PIN, I2S_LRCK_PIN, I2S_DATA_PIN);
     sampler->play();
 }
@@ -24,17 +24,6 @@ const float frequencies[] = {392.00, 440.00, 349.23, 174.61, 261.63};
 
 void loop()
 {
-    // controlPanel->loop();
-    /*
-
-    for (uint8_t i = 0; i < 5; i++)
-    {
-        synth->generateTone(frequencies[i], 1000, SINE);
-        delay(50);
-    }
-    delay(2000);
-    */
+    controlPanel->loop();
     sampler->loop();
-
-    // delay(2000);
 }
