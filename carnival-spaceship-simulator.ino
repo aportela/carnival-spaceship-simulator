4946119fa2c5645fd7e49cce24aceca04d10c27b
alex <766f6964+github@gmail.com>
2025-02-10 12:34:14 +0100

    added button test

--- carnival-spaceship-simulator.ino
+++ carnival-spaceship-simulator.ino
@@ -1,6 +1,8 @@
 #include "src/modules/TM1638plus/ModuleTM1638plus.hpp"
 #include "src/modules/Audio/Sampler.hpp"
+#include "src/modules/ExternalButtons/ExternalButtons.hpp"
 
+ExternalButtons *buttons = nullptr;
 ModuleTM1638plus *controlPanel = nullptr;
 Sampler *sampler = nullptr;
 
@@ -14,9 +16,12 @@ Sampler *sampler = nullptr;
 
 void setup()
 {
+    const uint8_t BUTTON_PINS[TOTAL_BUTTONS] = {11, 12, 13, 14, 15};
+    buttons = new ExternalButtons(BUTTON_PINS);
     Serial.begin(9600);
     controlPanel = new ModuleTM1638plus(STROBE_TM, CLOCK_TM, DIO_TM, true);
     sampler = new Sampler(I2S_BCK_PIN, I2S_LRCK_PIN, I2S_DATA_PIN);
+    Serial.println("Playing");
     sampler->play(SAMPLE_LASER1_SINGLE);
 }
 
@@ -24,6 +29,29 @@ const float frequencies[] = {392.00, 440.00, 349.23, 174.61, 261.63};
 
 void loop()
 {
+    switch (buttons->loop())
+    {
+    case EXTERNAL_BUTTON_1:
+        sampler->play(SAMPLE_LASER1_SINGLE);
+        Serial.println("BUTTON1");
+        break;
+    case EXTERNAL_BUTTON_2:
+        sampler->play(SAMPLE_LASER1_SINGLE);
+        Serial.println("BUTTON2");
+        break;
+    case EXTERNAL_BUTTON_3:
+        sampler->play(SAMPLE_LASER1_SINGLE);
+        Serial.println("BUTTON3");
+        break;
+    case EXTERNAL_BUTTON_4:
+        sampler->play(SAMPLE_LASER1_SINGLE);
+        Serial.println("BUTTON4");
+        break;
+    case EXTERNAL_BUTTON_5:
+        sampler->play(SAMPLE_LASER1_SINGLE);
+        Serial.println("BUTTON5");
+        break;
+    }
     controlPanel->loop();
     sampler->loop();
 }
