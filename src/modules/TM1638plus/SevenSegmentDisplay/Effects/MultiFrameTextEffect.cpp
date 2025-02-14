4fdb97e5b487a7d1369716a7e716769beeb46df3
alex <766f6964+github@gmail.com>
2025-02-14 23:35:55 +0100

    class implemented

--- src/modules/TM1638plus/SevenSegmentDisplay/Effects/MultiFrameTextEffect.cpp
+++ src/modules/TM1638plus/SevenSegmentDisplay/Effects/MultiFrameTextEffect.cpp
@@ -1,1 +1,47 @@
 #include "MultiFrameTextEffect.hpp"
+
+MultiFrameTextEffect::MultiFrameTextEffect(TM1638plus *module, const char *frames[], size_t frameCount, uint16_t frameTimeout, const uint8_t startIndex) : module(module), frameCount(frameCount), frameTimeout(frameTimeout), startIndex(startIndex)
+{
+    this->lastTimestamp = millis();
+    if (frames != nullptr && frameCount > 0)
+    {
+        this->frames = new const char *[frameCount];
+        for (size_t i = 0; i < frameCount; ++i)
+        {
+            size_t length = strlen(frames[i]) + 1;
+            this->frames[i] = new char[length];
+            strcpy(const_cast<char *>(this->frames[i]), frames[i]);
+        }
+    }
+}
+
+MultiFrameTextEffect::~MultiFrameTextEffect()
+{
+    if (this->frames)
+    {
+        for (size_t i = 0; i < frameCount; ++i)
+        {
+            delete[] this->frames[i];
+        }
+        delete[] this->frames;
+    }
+    this->frames = nullptr;
+}
+
+void MultiFrameTextEffect::loop(void)
+{
+    if (this->frames && this->frameCount > 0)
+    {
+        uint64_t currentMillis = millis();
+        if (currentMillis - this->lastTimestamp > this->frameTimeout)
+        {
+            this->lastTimestamp = currentMillis;
+            this->module->displayText(this->frames[this->currentFrameIndex]);
+            this->currentFrameIndex++;
+            if (this->currentFrameIndex >= this->frameCount)
+            {
+                this->currentFrameIndex = 0;
+            }
+        }
+    }
+}
--- src/modules/TM1638plus/SevenSegmentDisplay/Effects/MultiFrameTextEffect.hpp
+++ src/modules/TM1638plus/SevenSegmentDisplay/Effects/MultiFrameTextEffect.hpp
@@ -1,10 +1,21 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <TM1638plus.h>
 
 class MultiFrameTextEffect
 {
+private:
+    TM1638plus *module = nullptr;
+    const char **frames = nullptr;
+    size_t frameCount = 0;
+    uint16_t frameTimeout = 0;
+    uint8_t startIndex = 0;
+    uint8_t currentFrameIndex = 0;
+    uint64_t lastTimestamp = 0;
+
 public:
-    MultiFrameTextEffect(const char *frames[], size_t frameCount, uint16_t frameTimeout = 0, const uint8_t startIndex = 0);
+    MultiFrameTextEffect(TM1638plus *module, const char *frames[], size_t frameCount, uint16_t frameTimeout = 0, const uint8_t startIndex = 0);
     ~MultiFrameTextEffect();
 
     void loop(void);
-}
+};
