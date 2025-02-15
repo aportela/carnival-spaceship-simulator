87974821e0135cadd2a4c61767ee9b8cb1f82256
alex <766f6964+github@gmail.com>
2025-02-15 18:34:59 +0100

    type fixed

--- src/modules/TM1638plus/Leds/Effects/IntermitentLedEffect.cpp
+++ src/modules/TM1638plus/Leds/Effects/IntermitentLedEffect.cpp
@@ -5,7 +5,7 @@ const uint16_t IntermitentLedEffect::individualLedMasks[] = {
     LED_1 | LED_2 | LED_3 | LED_4 | LED_5 | LED_6 | LED_7 | LED_8,
 };
 
-IntermitentLedEffect::IntermitentLedEffect(TM1638plus *module, uint16_t msDelay) : LedEffect(module, IntermitentLedEffect::individualLedMasks, sizeof(IntermitentLedEffect::individualLedMasks) / sizeof(AlternateLedEffect::individualLedMasks[0]), msDelay)
+IntermitentLedEffect::IntermitentLedEffect(TM1638plus *module, uint16_t msDelay) : LedEffect(module, IntermitentLedEffect::individualLedMasks, sizeof(IntermitentLedEffect::individualLedMasks) / sizeof(IntermitentLedEffect::individualLedMasks[0]), msDelay)
 {
 }
 
