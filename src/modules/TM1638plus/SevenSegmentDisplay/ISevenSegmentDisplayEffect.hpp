383651b393c2733a1610f2b0ed88e14e6e66f23f
alex <766f6964+github@gmail.com>
2025-02-18 10:15:34 +0100

    added new virtual method for multi frame segments

--- src/modules/TM1638plus/SevenSegmentDisplay/ISevenSegmentDisplayEffect.hpp
+++ src/modules/TM1638plus/SevenSegmentDisplay/ISevenSegmentDisplayEffect.hpp
@@ -8,6 +8,7 @@ public:
     virtual void loop(void) = 0;
     virtual bool isSimpleTextEffect() { return false; }
     virtual bool isMultiFrameTextEffect() { return false; }
+    virtual bool isMultiFrameSegmentEffect() { return false; }
 };
 
 #endif // TM1638_PLUS_SEVEN_SEGMENT_DISPLAY_EFFECT_INTERFACE_H
